93449ffce82b43cbf7e331faa176fb17f13cb592
Jason Hamel <github@jasongets.email>
2023-02-13 19:43:55 -0500

    Reworked to use a Map for the dupe list to avoid n^2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        //String string = "GM,Ford,Tesla,Volkswagen,Audi,Subaru,Tesla,Ford,Ford,Subaru,Mercedes,Hyundai,Honda,Suzuki";
-        String string = "GM,GM";
+        String string = "GM,Ford,Tesla,Volkswagen,Audi,Subaru,Tesla,Ford,Ford,Subaru,Mercedes,Hyundai,Honda,Suzuki";
+        //String string = "GM,GM";
 
-        String[] seperatedString = string.split(",");
+        String[] separatedString = string.split(",");
         List<String> list = new ArrayList<>();
-        for (String s : seperatedString) {
+        for (String s : separatedString) {
             System.out.println(s);
             list.add(s);
         }
@@ -17,24 +18,28 @@ public class Main {
         System.out.println();
 
         list = list.stream().sorted().collect(Collectors.toList());
-        List<String> noDupes = new ArrayList<>();
+        HashMap<Integer, String> noDupes = new HashMap<>();
+        int key = 0;
         for (String s : list) {
             System.out.println(s);
-            if (!(noDupes.contains(s))) {
-                noDupes.add(s);
+            if (!(noDupes.containsValue(s))) {
+                noDupes.put(key, s);
+                key++;
             }
+
+
         }
 
         System.out.println();
 
-        for (String s : noDupes) {
-            System.out.println(s);
+        for (int i = 0; i < noDupes.size(); i++) {
+            System.out.println(noDupes.get(i));
         }
 
         System.out.println();
 
-        for (String s : noDupes) {
-            System.out.println(s.length());
+        for (int i = 0; i < noDupes.size(); i++) {
+            System.out.println(noDupes.get(i).length());
         }
     }
 }
